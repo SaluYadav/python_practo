6ca04c8b5b52858ac96f095c49536bc12d00b48b
Yadav <salu.yadav@revvity.com>
2026-06-12 22:57:59 +0530

    updated script

--- variables.py
+++ variables.py
@@ -2,4 +2,15 @@ Name = "salu Yadav"
 Age = 48
 
 Name : str = "salu Yadav"
+price = 100.50
 
+print(f"the price is Rs.{price}")
+
+is_student=False
+
+print(f'Are you a student?: {is_student}')
+
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")
